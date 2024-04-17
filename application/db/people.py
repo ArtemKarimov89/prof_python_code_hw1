8f1fa699e9f916bf2c43e0a8fd7625aaace9918a
Artem <karimov.art89@gmail.com>
2024-04-17 23:31:19 +0300

    Initial commit

---
+++ application/db/people.py
@@ -0,0 +1,6 @@
+import datetime
+
+
+def get_employees():
+    print(datetime.datetime.now())
+    print("Все сотрудники гуляют где-то")
---
+++ application/salary.py
@@ -0,0 +1,6 @@
+import datetime
+
+
+def calculate_salary(salary_per_hour):
+    print(datetime.datetime.now())
+    print(salary_per_hour * 168)
